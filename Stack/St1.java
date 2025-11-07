3b89dfe799cde0de7e524bd7b3bd9d5669191e66
shahiChiranjivi <163614892+shahiChiranjivi@users.noreply.github.com>
2025-11-07 14:43:05 +0545

    Added stack folder and first file
    Implemented basic stack operations including push, pop, and peek.

--- Stack/St1.java
+++ Stack/St1.java
@@ -1,1 +1,26 @@
+//Basics  & Implementation of stack 
 
+import java.util.*;
+
+public class St1{
+
+  public static void main(String[] args){
+  Stack<Integer> s = new Stack<>();
+  //number 1 operation(add)
+  s.push(1);//last item
+  s.push(2);//second last item
+  s.push(3);//last item always first to be removed
+
+  //number 2 operation pop(remove)
+  s.pop();
+ 
+ 
+  //System.out.println(s);
+  //number 3 operation peek(to see top element)
+  System.out.println(s.peek());//to check top element
+
+
+
+
+  }
+}
